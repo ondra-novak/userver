b6b16ca24c73bbe592cb9c7fe2ee0ba2616a5d03
Ondřej Novák <ondra-novak@email.cz>
2021-02-10 12:57:55 +0100

    comments

--- isocket.h
+++ isocket.h
@@ -15,19 +15,59 @@ namespace userver {
 class ISocket {
 public:
 
+	///Read to a buffer synchronously
+	/**
+	 * @param buffer pointer to a buffer
+	 * @param size size of the buffer
+	 * @return returns count of read bytes, returns 0 if there is end of stream or timeout.
+	 *
+	 * @note read is blocking, you need to use async version to perform nonblocking read
+	 */
 	virtual int read(void *buffer, unsigned int size) = 0;
+	///Write the buffer
+	/**
+	 *
+	 * @param buffer pointer to a buffer
+	 * @param size size of the buffer
+	 * @return returns count of written bytes. Returns 0 if write timeouted
+	 */
 	virtual int write(const void *buffer, unsigned int size) = 0;
+
+	///Read asynchronously
+	/**
+	 * @param buffer pointer to buffer
+	 * @param size size of the buffer
+	 * @param fn function called when read is done. Function receives count of bytes
+	 * actualy read to the buffer. The value has the same meaning as return value of
+	 * synchronized read.
+	 *
+	 * @note You must ensure, that buffer is valid during waiting for completion.
+	 */
 	virtual void read(void *buffer, unsigned int size, CallbackT<void(int)> &&fn) = 0;
+	///Write asynchronously
 	virtual void write(const void *buffer, unsigned int size, CallbackT<void(int)> &&fn) = 0;
 
+	///Close the output
+	/** when output is closed, futher writes are rejected. The other side receives
+	 * EOF, however, reading is still possible
+	 */
 	virtual void closeOutput() = 0;
+	///Close the input
+	/** when input is closed, any futher reads will return EOF. Other side will be unable
+	 * to send data and connection will look closed. However writing is still possible
+	 */
 	virtual void closeInput() = 0;
 
+	///Set read timeout (in ms)
 	virtual void setRdTimeout(int tm) = 0;
+	///Set write timeout (in ms)
 	virtual void setWrTimeout(int tm) = 0;
+	///Set both read and write timeout (in ms)
 	virtual void setIOTimeout(int tm) = 0;
 
+	///Get current read timeout
 	virtual int getRdTimeout() const = 0;
+	///Get current write timeout
 	virtual int getWrTimeout() const = 0;
 
 	///Wait for connect
@@ -49,6 +89,7 @@ public:
 
 	virtual ~ISocket() {}
 
+	///returns true, when any blocking or async operation timeouted
 	virtual bool timeouted() const = 0;
 };
 
